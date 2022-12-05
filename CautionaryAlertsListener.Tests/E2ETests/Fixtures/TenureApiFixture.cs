08d2dda27f7f53db35bc78b50a2a4d6498cc76fb
Ognyan Aleksiev <ognyan.aleksiev@foundry4.com>
2022-12-05 16:18:32 +0200

    Tenure fixtures

---
+++ CautionaryAlertsListener.Tests/E2ETests/Fixtures/TenureApiFixture.cs
@@ -0,0 +1,108 @@
+using AutoFixture;
+using CautionaryAlertsListener.Boundary;
+using CautionaryAlertsListener.Domain;
+using Force.DeepCloner;
+using Hackney.Core.Testing.Shared.E2E;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CautionaryAlertsListener.Tests.E2ETests.Fixtures
+{
+    public class TenureApiFixture : BaseApiFixture<TenureInformation>
+    {
+        private readonly Fixture _fixture = new Fixture();
+        private const string DateFormat = "yyyy-MM-ddTHH\\:mm\\:ss.fffffffZ";
+
+        public EventData MessageEventData { get; private set; }
+        public Guid AddedPersonId { get; private set; }
+        public Guid RemovedPersonId { get; private set; }
+
+        public TenureApiFixture()
+            : base(FixtureConstants.TenureApiRoute, FixtureConstants.TenureApiToken)
+        {
+            Environment.SetEnvironmentVariable("TenureApiUrl", FixtureConstants.TenureApiRoute);
+            Environment.SetEnvironmentVariable("TenureApiToken", FixtureConstants.TenureApiToken);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && !_disposed)
+            {
+                base.Dispose(disposing);
+            }
+        }
+
+        private List<HouseholdMembers> CreateHouseholdMembers(int count = 3)
+        {
+            return _fixture.Build<HouseholdMembers>()
+                           .With(x => x.Id, () => Guid.NewGuid().ToString())
+                           .With(x => x.DateOfBirth, DateTime.UtcNow.AddYears(-40).ToString(DateFormat))
+                           .With(x => x.PersonTenureType, "Tenant")
+                           .CreateMany(count).ToList();
+        }
+
+        private void CreateMessageEventDataForPersonAdded(List<HouseholdMembers> hms = null)
+        {
+            var oldData = hms ?? CreateHouseholdMembers();
+            var newData = oldData.DeepClone();
+
+            var newHm = CreateHouseholdMembers(1).First();
+            newData.Add(newHm);
+            AddedPersonId = Guid.Parse(newHm.Id);
+
+            MessageEventData = new EventData()
+            {
+                OldData = new Dictionary<string, object> { { "householdMembers", oldData } },
+                NewData = new Dictionary<string, object> { { "householdMembers", newData } }
+            };
+        }
+
+        private void CreateMessageEventDataForPersonRemoved(Guid id)
+        {
+            var oldData = CreateHouseholdMembers();
+            var newData = oldData.DeepClone();
+
+            var removedHm = CreateHouseholdMembers(1).First();
+            removedHm.Id = id.ToString();
+            oldData.Add(removedHm);
+            RemovedPersonId = id;
+
+            MessageEventData = new EventData()
+            {
+                OldData = new Dictionary<string, object> { { "householdMembers", oldData } },
+                NewData = new Dictionary<string, object> { { "householdMembers", newData } }
+            };
+        }
+
+        public void GivenTheTenureDoesNotExist(Guid id)
+        {
+            CreateMessageEventDataForPersonAdded();
+        }
+
+        public void GivenAPersonWasRemoved(Guid id)
+        {
+            CreateMessageEventDataForPersonRemoved(id);
+        }
+
+        public TenureInformation GivenTheTenureExists(Guid id)
+        {
+            return GivenTheTenureExists(id, null);
+        }
+        public TenureInformation GivenTheTenureExists(Guid id, Guid? personId)
+        {
+            ResponseObject = _fixture.Build<TenureInformation>()
+                                     .With(x => x.Id, id.ToString())
+                                     .With(x => x.StartOfTenureDate, DateTime.UtcNow.AddMonths(-6).ToString(DateFormat))
+                                     .With(x => x.EndOfTenureDate, DateTime.UtcNow.AddYears(6).ToString(DateFormat))
+                                     .With(x => x.HouseholdMembers, CreateHouseholdMembers())
+                                     .Create();
+
+            if (personId.HasValue)
+                ResponseObject.HouseholdMembers.First().Id = personId.ToString();
+
+            CreateMessageEventDataForPersonAdded(ResponseObject.HouseholdMembers);
+            return ResponseObject;
+        }
+    }
+}
---
+++ CautionaryAlertsListener.Tests/FixtureConstants.cs
@@ -0,0 +1,8 @@
+namespace CautionaryAlertsListener.Tests
+{
+    public static class FixtureConstants
+    {
+        public static string TenureApiRoute => "http://localhost:5778/api/v1/tenures/";
+        public static string TenureApiToken => "sdjkhfgsdkjfgsdjfgh";
+    }
+}
