583679ab57e7cfd4d919d901104c7b73d7ae78d7
Ognyan Aleksiev <ognyan.aleksiev@foundry4.com>
2022-12-05 18:21:25 +0200

    another collection of removed default files

--- CautionaryAlertsListener/Boundary/EventData.cs
+++
@@ -1,9 +0,0 @@
-namespace CautionaryAlertsListener.Boundary
-{
-    public class EventData
-    {
-        public object OldData { get; set; }
-
-        public object NewData { get; set; }
-    }
-}
