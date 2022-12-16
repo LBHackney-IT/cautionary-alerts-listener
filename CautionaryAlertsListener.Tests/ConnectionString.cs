a0a56babed0f40201d7a66fe27087a609dde8c85
Ognyan Aleksiev <ognyan.aleksiev@foundry4.com>
2022-12-16 15:14:39 +0200

    update connection string ource for tests

--- CautionaryAlertsListener.Tests/ConnectionString.cs
+++ CautionaryAlertsListener.Tests/ConnectionString.cs
@@ -6,11 +6,7 @@ namespace CautionaryAlertsListener.Tests
     {
         public static string TestDatabase()
         {
-            return $"Host={Environment.GetEnvironmentVariable("DB_HOST") ?? "127.0.0.1"};" +
-                   $"Port={Environment.GetEnvironmentVariable("DB_PORT") ?? "5432"};" +
-                   $"Username={Environment.GetEnvironmentVariable("DB_USERNAME") ?? "postgres"};" +
-                   $"Password={Environment.GetEnvironmentVariable("DB_PASSWORD") ?? "mypassword"};" +
-                   $"Database={Environment.GetEnvironmentVariable("DB_DATABASE") ?? "testdb"}";
+            return Environment.GetEnvironmentVariable("CONNECTION_STRING");
         }
     }
 }
